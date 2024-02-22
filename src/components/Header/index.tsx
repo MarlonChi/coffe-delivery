7ecb31d085468f85539adbc946257cd53544fdd0
MarlonChi <marlonchiodelli@hotmail.com>
2024-02-22 15:43:04 -0300

    feat: header address

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { MapPin, ShoppingCart } from "phosphor-react";
 
 import { CartContext } from "../../contexts/CartContext";
+import { UserContext } from "../../contexts/UserContext";
 
 import LogoImg from "../../assets/logo.svg";
 
@@ -10,6 +11,7 @@ import * as S from "./styles";
 
 export const Header = () => {
   const { products } = useContext(CartContext);
+  const { addressData } = useContext(UserContext);
 
   const totalQuantity = products.reduce((acc, item) => {
     return acc + (item.quantity || 0);
@@ -21,10 +23,12 @@ export const Header = () => {
         <img src={LogoImg} />
       </NavLink>
       <nav>
-        <NavLink to="/checkout">
-          <MapPin size={22} weight="fill" />
-          Erechim, RS
-        </NavLink>
+        {addressData && (
+          <NavLink to="/checkout">
+            <MapPin size={22} weight="fill" />
+            {addressData.city}, {addressData.state}
+          </NavLink>
+        )}
         <NavLink to="/checkout">
           <S.Cart>
             <ShoppingCart size={22} weight="fill" />
